e54d7bd1d76b8de3cb352a8d3aa263a1a0124837
nilsw13 <nils.wenting@laplateforme.io>
2025-10-20 21:47:39 +0200

    feature (frontend) : Dynamisation header en composant

---
+++ frontend/src/scripts/header.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const headerElement = document.getElementsByClassName('header')[0];
+
+    const headerHtmlContent = `
+        <div class="header-container">
+            <a href="index.html" class="logo">
+                <img src="./src/assets/CineVerse.svg" alt="Logo Cineverse">
+            </a>
+            
+            <nav class="navbar">
+                <ul class="nav-links">
+                    <li><a href="#" class="active">Accueil</a></li>
+                    <li><a href="#">Catalogue</a></li>
+                    <li><a href="#">Artistes</a></li>
+                    <li><a href="#">À propos</a></li>
+                </ul>
+            </nav>
+            
+            <div class="search-wrapper">
+                <button class="search-btn" aria-label="Lancer la recherche">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+                <input type="text" class="search-input" placeholder="Rechercher un film, un artiste..." aria-label="Rechercher">
+            </div>
+            
+            <div class="burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        
+        <div class="mobile-menu">
+            <div class="mobile-search">
+                <button class="search-btn">
+                    <i class="fas fa-search"></i>
+                </button>
+                <input type="text" placeholder="Rechercher...">
+            </div>
+            
+            <ul>
+                <li><a href="#" class="active">Accueil</a></li>
+                <li><a href="#">Catalogue</a></li>
+                <li><a href="#">Artistes</a></li>
+                <li><a href="#">À propos</a></li>
+            </ul>
+        </div>
+        
+        <hr class="borderheader">
+    `;
+
+    if (headerElement) {
+        headerElement.innerHTML = headerHtmlContent;
+    }
+});
