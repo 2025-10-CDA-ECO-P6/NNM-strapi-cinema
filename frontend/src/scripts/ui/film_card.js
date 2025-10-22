6529b3305b3ac7912ba8fc82c2b3a4a98097115e
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-10-22 12:13:53 +0200

    feat: artiste details and routing
    routing of specifique movies from artist and fix on css style
    
    verified

--- frontend/src/scripts/ui/film_card.js
+++ frontend/src/scripts/ui/film_card.js
@@ -1,6 +1,10 @@
-export function createFilmCard(movie) {
+export function createFilmCard(movie, isLink = true) {
   const card = document.createElement("div");
   card.className = "film-card";
+
+  const container = isLink ? document.createElement("a") : document.createElement("div");
+  container.className = isLink ? "artiste-link" : "artiste-container";
+  if (isLink) container.href = `film.html?id=${movie.id}`;
 
   const img = document.createElement("img");
   img.src = movie.poster_image || "./src/assets/placeholder.webp";
@@ -9,16 +13,15 @@ export function createFilmCard(movie) {
   img.style.borderRadius = "6px";
   card.appendChild(img);
 
-
   const title = document.createElement("h4");
   title.textContent = movie.title || "Titre inconnu";
-  title.className = "featured-label small-title";
+  title.className = "small-title";
   card.appendChild(title);
 
   const releaseDate = document.createElement("p");
   releaseDate.textContent = `Date de sortie : ${movie.release_date || "N/A"}`;
   card.appendChild(releaseDate);
 
-
-  return card;
+  container.appendChild(card);
+  return container;
 }
