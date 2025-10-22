378eadd1407ade096ec189c718d6e549ceb4d5d4
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-10-22 11:41:56 +0200

    chores: wip

--- frontend/src/scripts/ui/film_card.js
+++ frontend/src/scripts/ui/film_card.js
@@ -2,20 +2,23 @@ export function createFilmCard(movie) {
   const card = document.createElement("div");
   card.className = "film-card";
 
-  const title = document.createElement("h4");
-  title.textContent = movie.title || "Titre inconnu";
-  card.appendChild(title);
-
-  const releaseDate = document.createElement("p");
-  releaseDate.textContent = `Date de sortie : ${movie.release_date || "N/A"}`;
-  card.appendChild(releaseDate);
-
   const img = document.createElement("img");
   img.src = movie.poster_image || "./src/assets/placeholder.webp";
   img.alt = movie.title || "Affiche du film";
   img.style.width = "100%";
   img.style.borderRadius = "6px";
   card.appendChild(img);
+
+
+  const title = document.createElement("h4");
+  title.textContent = movie.title || "Titre inconnu";
+  title.className = "featured-label small-title";
+  card.appendChild(title);
+
+  const releaseDate = document.createElement("p");
+  releaseDate.textContent = `Date de sortie : ${movie.release_date || "N/A"}`;
+  card.appendChild(releaseDate);
+
 
   return card;
 }
