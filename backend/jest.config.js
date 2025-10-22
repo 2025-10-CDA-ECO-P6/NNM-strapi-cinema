acc1406e231d8d59fff964f999249adccef37af3
nilsw13 <nils.wenting@laplateforme.io>
2025-10-22 15:10:08 +0200

    test (us-06) : Set up test env

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -9,6 +9,13 @@ module.exports = {
   //  Dossier racine des tests :
   // roots: ["<rootDir>/tests"],
 
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/.tmp/", // Fichiers temporaires de Strapi
+    "/.cache/", // Cache de Strapi
+    "/dist/" // Fichiers de build
+  ],
+
   moduleFileExtensions: ["js", "json", "ts"],
   transform: {
     ...tsJestTransformCfg,
@@ -17,6 +24,15 @@ module.exports = {
 
   // 🧪 Collecte de la couverture de code
   collectCoverage: true,
-  collectCoverageFrom: ["scripts/**/*.js"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,js}", // Tous les fichiers .ts et .js dans src
+    "!src/**/*.test.{ts,js}",
+    "!src/admin/**", // Exclut l'interface d'administration
+    // Exclut les configurations et types générés qui ne sont pas de la logique métier
+    "!src/index.ts",
+    "!src/register.ts",
+    "!src/bootstrap.ts",
+    "!src/config/**"
+  ],
   coverageDirectory: "coverage",
 };
